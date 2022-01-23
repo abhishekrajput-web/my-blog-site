6b2bfcc05542ea6ba79c6e16f25dc4b13294719a
Abhishek Rajput <78433992+abhishekrajput-web@users.noreply.github.com>
2022-01-23 05:37:03 -0700

    uploading blogs files

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navContainer = document.querySelector(".nav-container");
+const navLinks = document.querySelectorAll(".nav-link");
+const btnGrop = document.querySelectorAll(".btn");
+navToggle.addEventListener("click", function () {
+  navContainer.classList.toggle("show");
+  console.log("hello world");
+});
+
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    navContainer.classList.toggle("show");
+  });
+
+});
