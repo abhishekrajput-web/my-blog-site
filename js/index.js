4da5f280a90334a06bf7084402bf3b5eda08e723
Abhishek Rajput <78433992+abhishekrajput-web@users.noreply.github.com>
2022-01-27 03:45:15 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -19,3 +19,21 @@ const preloader = document.querySelector(".preloader");
 window.addEventListener("load", function() {
     preloader.classList.add("hide-preloader");
 });
+
+// on scroll btn
+const mybutton = document.getElementById("myBtn");
+mybutton.addEventListener("click", topFunction);
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
